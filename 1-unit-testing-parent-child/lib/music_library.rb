25c441f986a4516e492253e173fd093a4529eaf1
Sean Peters <sp@snptrs.com>
2023-05-16 17:39:38 +0100

    Add integration mocking test for adding track

--- 1-unit-testing-parent-child/lib/music_library.rb
+++ 1-unit-testing-parent-child/lib/music_library.rb
@@ -1,19 +1,18 @@
 class MusicLibrary
   def initialize
-    # ...
+    @tracks = []
   end
 
   def add(track) # track is an instance of Track
-    # Track gets added to the library
-    # Returns nothing
+    @tracks << track
+    binding.irb
   end
 
   def all
-    # Returns a list of track objects
+    @tracks
   end
   
   def search(keyword) # keyword is a string
-    # Returns a list of tracks that match the keyword
+    @tracks.map(&:match?(keyword))
   end
 end
-
