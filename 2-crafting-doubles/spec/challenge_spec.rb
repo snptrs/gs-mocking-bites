10a9924dd6f23216e8ee1dba970fadf97d412c49
Sean Peters <sean@snptrs.dev>
2023-05-17 16:40:09 +0100

    Completes challenge

--- 2-crafting-doubles/spec/challenge_spec.rb
+++ 2-crafting-doubles/spec/challenge_spec.rb
@@ -1,7 +1,12 @@
 RSpec.describe "doubles challenge" do
   it "creates a sophisticated double" do
-    task_list = # ...
-    task = # ...
+    task_list = double :task_list
+    task = double :task
+    allow(task_list).to receive(:add)
+    allow(task_list).to receive(:list).and_return([task])
+    allow(task_list).to receive(:count).and_return(1)
+    allow(task_list).to receive(:clear).and_return(:success)
+
 
     # Don't edit below
     task_list.add(task)
