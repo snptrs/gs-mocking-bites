e3239f527f5cd9aba5c7a2c20ca28bfab99980e9
Sean Peters <sean@snptrs.dev>
2023-05-17 14:40:28 +0100

    Adds tests to task_list_spec

--- 1-unit-testing-parent-child/challenge/spec/task_list_spec.rb
+++ 1-unit-testing-parent-child/challenge/spec/task_list_spec.rb
@@ -11,5 +11,23 @@ RSpec.describe TaskList do
     expect(task_list.all_complete?).to eq false
   end
 
-  # Unit test `#all` and `#all_complete?` behaviour
+  it "adds and returns tasks" do
+    task_list = TaskList.new
+    task_1 = double :task
+    task_2 = double :task
+    task_list.add(task_1)
+    task_list.add(task_2)
+    expect(task_list.all).to eq [task_1, task_2]
+  end
+
+  it "confirms all tasks are complete" do
+    task_list = TaskList.new
+    task_1 = double :task
+    expect(task_1).to receive(:complete?).and_return(true)
+    task_2 = double :task
+    expect(task_2).to receive(:complete?).and_return(true)
+    task_list.add(task_1)
+    task_list.add(task_2)
+    expect(task_list.all_complete?).to eq true
+  end
 end
